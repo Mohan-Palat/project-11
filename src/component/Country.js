865e5c24002386abb2a5f9e1db5a8efa3a3f7031
Jessie Chen <yhcjessie1@gmail.com>
2020-11-19 10:45:26 -0500

    component setup

---
+++ src/component/Country.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+class Country extends Component {
+ 
+    render() { 
+        return ( 
+            <>
+            <p>dropdown menu for based country</p>
+            <p>dropdown menu for Compared country</p>
+            </>
+         );
+    }
+}
+ 
+export default Country;
