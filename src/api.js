c9188a251781811806ceab89a81467fb88ce0c48
Lawrence DeMaio <ldemaio121@hotmail.com>
2020-11-19 12:10:52 -0500

    Set up dropdown to include currency codes and descriptions

--- src/api.js
+++ src/api.js
@@ -2,10 +2,12 @@ import axios from 'axios'
 
 const getSymbols = ()=> {
     axios.default.baseURL = process.env.REACT_APP_BASE_URL_SYMBOLS
-    return axios.get(`/api/symbols?access_key=${process.env.REACT_APP_API_KEY_SYMBOLS}`)
+    return axios.get(`http://data.fixer.io/api/symbols?access_key=${process.env.REACT_APP_API_KEY_SYMBOLS}`)
 }
 
 const getExchangeRate= (base)=> {
     axios.default.baseURL = process.env.REACT_APP_BASE_URL_EXCHANGE
     return axios.get(`/v6/${process.env.REACT_APP_API_KEY_EXCHANGE}/latest/${base}`)
 }
+
+export { getSymbols, getExchangeRate };
--- src/component/Country.js
+++ src/component/Country.js
@@ -1,9 +1,51 @@
 import React, { Component } from 'react';
+import {getSymbols} from '../api.js';
+
 class Country extends Component {
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            countryMap: ({})
+        }
+    }
+
+    componentDidMount() {
+        getSymbols()
+          .then((response) => {
+            console.log('Country codes', response.data.symbols);
+
+            this.setState({
+                countryMap: response.data.symbols
+            })
+    
+          })
+          .catch((error) => {
+            console.log('API ERROR:', error);
+          });
+    };
  
     render() { 
+        //puts list of countries in array and then maps them into dropdown options
+        let countries = [];
+        for (var key in this.state.countryMap) 
+            countries.push( `${key} - ${this.state.countryMap[key]}` );
+        
+        const options = countries.map(country => {
+            return <option key={country.slice(0, 3)} value={country.slice(0, 3)}> {country} </option>;
+          });
+    
+        
+
         return ( 
             <>
+
+            <select id="cars" name="cars">
+                
+            {options}
+            </select>
+
             <p>dropdown menu for based country</p>
             <p>dropdown menu for Compared country</p>
             </>
